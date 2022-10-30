c78685dd4b14664185364dadc8cd1fe474fa6cdc
kayteawest <kaytea.westcott@gmail.com>
2022-10-29 19:13:34 -0600

    Update

--- src/main/resources/projects-schema.sql
+++ src/main/resources/projects-schema.sql
@@ -25,7 +25,7 @@ CREATE TABLE project_category (
 	project_id INT NOT NULL,
 	category_id INT NOT NULL,
 	FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE,
-	FOREIGN KEY (category_id) REFERENCES project (project_id) ON DELETE CASCADE,
+	FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE,
 	UNIQUE KEY (project_id, category_id)
 );
 
